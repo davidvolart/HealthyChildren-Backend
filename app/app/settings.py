1b989de2228d07d969f1c23b918227e2058d1dff
david <davidvolart1997@gmail.com>
2020-08-31 23:57:45 +0200

    add authentication

--- app/app/settings.py
+++ app/app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'core',
     'user',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -127,3 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'core.User'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
--- app/user/views.py
+++ app/user/views.py
@@ -1,5 +1,9 @@
 from rest_framework import generics
 from user.serializers import UserSerializer
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -7,3 +11,13 @@ class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
+@api_view(['POST'])
+def authenticate_user(request):
+    validated_email = request.data.get("email")
+    validated_password = request.data.get("password")
+
+    user = authenticate(username=validated_email, password=validated_password)
+    if user is not None:
+        return Response({"message": "User authenticated"}, status=status.HTTP_200_OK)
+    else:
+        return Response({"message": "User doesn't exists"}, status=status.HTTP_404_NOT_FOUND)
